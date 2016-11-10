f18b37f92815dc3a8e4000c5a09d90c33e388b0e
Daria <dkravets@ukr.net>
2016-11-10 19:26:17 +0200

    Валидация(регистрация)
    Валидация e-mail, пароля и логина при регистрации

--- web/register.php
+++ web/register.php
@@ -18,10 +18,27 @@ if (isset($_POST['email'])) { $email = $_POST['email']; if ($email == '') { unse
     $login = trim($login);
     $password = trim($password);
     $r_password = trim($r_password);
+
 include ("db.php");
     $result = mysql_query("SELECT id FROM users WHERE login='$login'",$conn);
     $myrow = mysql_fetch_array($result);
-    if (!empty($myrow['id'])) {
+
+preg_match('/^([\w\.]{3,})@([\w\.]{1,}\.[\w]{1,})$/', $email, $matchemail);
+if (!($matchemail)){
+echo "Вы ввели не email. Пожалуйста, будьте аккуратней.";
+    exit();}
+
+preg_match('/^[a-z0-9_-]{4,16}$/',$password, $matchpass);
+if (!($matchpass)){
+echo "Пароль может содержать латинские буквы, цифры, дефисы, подчеркивания и быть длинной от 4 до 16 символов.";
+    exit();}
+
+preg_match('/^[a-z0-9_-]{1,20}$/',$login, $matchlog);
+if (!($matchlog)){
+echo "Логин может содержать латинские буквы, цифры, дефисы, подчеркивания и быть длинной от 1 до 20 символов.";
+    exit();}
+
+if (!empty($myrow['id'])) {
     echo "Извините, введённый вами логин уже зарегистрирован. Введите другой логин.";
         exit();
     }
